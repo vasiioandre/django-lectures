6073d624b70808373ccef355c5ba0d0d08bd8288
vasiioandre <vasi_ioandre@yahoo.com>
2023-08-02 23:41:16 +0300

    User Authentication and Sessions

---
+++ library/catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+    path('create_book/', views.BookCreate.as_view(), name='create_book'),
+    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail'),
+    path('my_view/', views.my_view, name='my_view'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('profile/', views.CheckedOutBooksByUserView.as_view(), name='profile')
+]
